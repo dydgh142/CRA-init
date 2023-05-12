b7b4aecd246f2d50471d52d976fcf3126fc27d13
yoonho <dydgh179@gmail.com>
2023-05-12 23:10:07 +0900

    [feat] 기본 환경구성 마무리 (페이지 연결 정상작동)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Main from "./pages/Main/Main";
+import Test from "./pages/Test/Test";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Test/Test.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class test extends Component {
+  render() {
+    return <div>테스트 페이지입니다.</div>;
+  }
+}
+
+export default test;
