b01dd1954eb9692d1f6a0e454dc0ec3e193a5151
yoonho <dydgh179@gmail.com>
2023-05-18 00:31:21 +0900

    [fix] header 기능 변경

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,32 +5,28 @@ import "./search.css";
 
 const Header = () => {
   return (
-    <>
-      <div className="relative h-[50px]">
-        <header className="fixed h-[50px] w-full bg-white z-20 border-b-2">
-          <span className="w-36 absolute top-[50%] translate-y-[-50%] left-[20vw] max-sm:left-[5vw]">
-            <a className="flex flex-col" href="https://www.instagram.com/strap_m_/" target="_blank">
-              <div className="font-bold">
-                <FaCameraRetro className="w-8 h-6 inline-block" />
-                strap_m_
-              </div>
-            </a>
-          </span>
-          <span className="absolute top-[50%] -translate-y-[50%] font-bold">
-            <Link to={"/"} type="button" className="">
-              홈
-            </Link>
-            <Link to={"/intro"} type="button" className="mx-10">
-              소개
-            </Link>
-            <form method="get" action="http://www.google.co.kr/search" target="_blank" className="search-box absolute right-[5%]">
-              <input name="q" type="text" placeholder=" " />
-              <button type="reset" value={"구글검색"}></button>
-            </form>
-          </span>
-        </header>
+    <header className="fixed h-[50px] w-full bg-white flex flex-nowrap  z-20 border-b-2">
+      <div className="absolute left-[10vw] flex top-[50%] translate-y-[-50%] font-bold">
+        <a className="flex flex-col" href="https://www.instagram.com/strap_m_/" target="_blank">
+          <div className="">
+            <FaCameraRetro className="w-8 h-6 inline-block" />
+            strap_m_
+          </div>
+        </a>
+        <Link to={"/"} type="button" className="mx-10">
+          홈
+        </Link>
+        <Link to={"/intro"} type="button" className="">
+          소개
+        </Link>
       </div>
-    </>
+      <div className="absolute right-[10vw] top-[50%] -translate-y-[50%] font-bold">
+        <form method="get" action="http://www.google.co.kr/search" target="_blank" className="search-box absolute right-[5%]">
+          <input name="q" type="text" placeholder=" " />
+          <button type="reset" value={"구글검색"}></button>
+        </form>
+      </div>
+    </header>
   );
 };
 
