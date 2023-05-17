29e4878aabc4cf6ac99faf2423fd51dd922a2de1
yoonho <dydgh179@gmail.com>
2023-05-17 16:10:50 +0900

    [feat] header - 버튼추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { FaCameraRetro } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
       <div className="relative">
         <header className="fixed h-[50px] w-full bg-white z-20 border-b-2">
-          <div className="w-36 absolute top-[50%] translate-y-[-50%] left-[20vw] max-sm:left-[5vw]">
+          <span className="w-36 absolute top-[50%] translate-y-[-50%] left-[20vw] max-sm:left-[5vw]">
             <a className="flex flex-col" href="https://www.instagram.com/strap_m_/" target="_blank">
               <div className="font-bold">
                 <FaCameraRetro className="w-8 h-6 inline-block" />
                 strap_m_
               </div>
             </a>
-          </div>
+          </span>
+          <span className="absolute top-[50%] -translate-y-[50%] font-bold">
+            <Link to={"/"} type="button" className="mx-10">
+              홈
+            </Link>
+            <Link to={"/yoonho"} type="button">
+              소개
+            </Link>
+          </span>
         </header>
       </div>
     </>
