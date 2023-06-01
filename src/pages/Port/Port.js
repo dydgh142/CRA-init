87fc4778d885be444653e4d5e570abdab91b95f2
yoonho <dydgh179@gmail.com>
2023-06-01 12:14:23 +0900

    [feat] 포트폴리오 완성

--- src/pages/Port/Port.js
+++ src/pages/Port/Port.js
@@ -67,6 +67,132 @@ const Port = () => {
           프로젝트 타잉의 GitHub
         </a>
       </section>
+      <section className="w-[70%] mx-auto py-8 pb-14 border-b-2 border-gray-200">
+        <h3 className="text-2xl text-textbase font-semibold mb-2">
+          운동을 통한 캐릭터 육성 어플{" "}
+          <a href="https://github.com/fitnesssumm" target="_blank" className="textbutton">
+            ‘FitSum’
+          </a>
+        </h3>
+        <div className="text-gray-500 mb-6">참여인원: 총 2명</div>
+        <div className="font-bold text-2xl text-textbase mb-2">Description</div>
+        <div className="border-l-[3px] pl-4 border-textbase mb-5">2022.05. - 2022.11. (30주)</div>
+        <div className="font-bold text-xl text-textbase mb-2">Contents</div>
+        <div className="mb-2">📌2022 안양대학교 소프트웨어학과 Capstion Desgin 경진대회 참가</div>
+        <div className="mb-5">
+          🎖️2022 안양대학교 소프트웨어학과 Capstion Desgin 경진대회 <strong>장려상 </strong>수상
+        </div>
+        <div className="font-bold text-xl text-textbase mb-2">Summary</div>
+        <div className="w-full">
+          <img src="img/fitsumContnet.jpg" alt="핏썸 설명" className="w-[60%] mx-auto" />
+        </div>
+        <div className="my-3">
+          <div className="text-center text-textbase font-semibold text-lg">
+            <h4 className="mt-3 ">TensorFlow 의 PoseNet을 이용하여 사용자세를 인식 하며</h4>
+            <h4 className="mb-3 ">동작을 카운트해 주는 Application</h4>
+          </div>
+          <li className="my-1">주요 기능은 사용자가 개인의 캐릭터를 변화시키는 모습으로 운동후 캐릭터의 자세가 변경되고. 운동을 통해 모은 코인으로 캐릭터의 옷을 변경하는 등</li>
+          <li className="my-1">캐릭터 변경 뿐 아니라 다양한 동기부여 환경이 조성되어 있음</li>
+          <div className="font-semibold my-2">개발목표 3가지</div>
+          <li className="my-1">사용자가 정확한 자세로 운동을 했을 시에만 카운트가 되도록 함</li>
+          <li className="my-1">운동기구가 아닌 맨몸운동이 주가 되도록 함</li>
+          <li className="my-1">사용자가 운동을 지속적으로 할 수 있도록 동기부여 환경을 제공함</li>
+        </div>
+        <div className="my-3 mb-10">
+          <div className="font-semibold mb-2">YoonHo’s role</div>
+          <li className="my-1">figma 를 이용해 Application UI 디자인</li>
+          <li className="my-1">
+            <a src="https://www.tensorflow.org/lite/examples/pose_estimation/overview?hl=ko" target="_blank" className="textbutton">
+              Tensorflow-PoseNet
+            </a>
+            을 이용하여 사용자의 자세를 인식
+          </li>
+          <li className="my-1">
+            <a src="https://github.com/dydgh142/Fitsum_Android/blob/main/fitsum/posenet/RepetitionCounter.kt" target="_blank" className="textbutton">
+              관절 포인트의 움직임
+            </a>
+            을 이용하여 운동횟수 카운트
+          </li>
+          <li className="my-1">Dto와 RetrofitAPI 를 이용해 비동기적 으로 데이터 처리를 처리하고, 서버측에서 자동으로 파싱하게되어 빠른 통신과 좋은 가독성을 갖게 함</li>
+          <li className="my-1">JWTToken을 이용하여 자동로그인, 로그인상태 유지 등 구현</li>
+          <li className="my-1">캐릭터의 성별, 아이템 구매에 따른 이미지 변경기능으로 사용자의 재미를 더함</li>
+        </div>
+        <a href="https://github.com/fitnesssumm" target="_blank" className="textbutton text-lg">
+          프로젝트 FitSum의 GitHub
+        </a>
+      </section>
+      <section className="w-[70%] mx-auto py-8 pb-14 ">
+        <h3 className="text-2xl text-textbase font-semibold mb-2">
+          추억과 기억, 음악일기{" "}
+          <a href="https://github.com/dydgh142/BERMUDA_Android" target="_blank" className="textbutton">
+            'BERMUDA'
+          </a>
+        </h3>
+        <div className="text-gray-500 mb-6">참여인원: 총 6명</div>
+        <div className="font-bold text-2xl text-textbase mb-2">Description</div>
+        <div className="border-l-[3px] pl-4 border-textbase mb-5">2022.07. - 2022.10. (12주)</div>
+        <div className="font-bold text-xl text-textbase mb-2">Contents</div>
+        <div className="mb-2">📌2022 한국관광공사 카카오 주관 공모전 참가</div>
+        <div className="mb-5">
+          🎖️2022 한국관광공사 카카오 주관 ‘2022 관광데이터 활용 공모전 <strong>장려상 </strong>수상
+        </div>
+        <div className="font-bold text-xl text-textbase mb-2">Summary</div>
+        <div className="w-full">
+          <img src="img/bermuda_diagram.png" alt="핏썸 설명" className="w-[60%] mx-auto" />
+        </div>
+        <div className="my-3">
+          <div className="text-center text-textbase font-semibold text-lg">
+            <h4 className="my-3 ">사용자가 일기작성시 감성분석을 통하여 알맞은 음악을 추천해 주는 Application</h4>
+          </div>
+          <li className="my-1">사용자들이 사진및 영상을 통해 정보를 공유하는 일반적인 sns와 차별화를 두기 위해 글과 음악만을 통하여 감정을 공유하는 Application</li>
+          <div className="font-semibold my-2">카카오와 관광공사의 API를 사용</div>
+          <li className="my-1">kakao → 카카오 로그인, 카카오 맵 사용.</li>
+          <li className="my-1">한국관광공사 → 지역 별 행사 및 관광 정보 크롤링</li>
+        </div>
+        <div className="my-3 mb-10">
+          <div className="font-semibold mb-2">YoonHo’s role</div>
+          <li className="my-1">figma 를 이용해 Application UI 디자인</li>
+          <li className="my-1">Android Stuido를 사용하여 UI 개발 및 통신 구현 진행 </li>
+          <li className="my-1">Dto와 RetrofitAPI 를 이용해 비동기적 으로 데이터 처리를 처리하고, 서버측에서 자동으로 파싱하게되어 빠른 통신과 좋은 가독성을 갖게 함</li>
+          <li className="my-1">JWT Token을 이용하여 자동로그인, 로그인상태 유지 등 구현</li>
+          <li className="my-1">지역별관광 데이터를 이용해 축제정보, 관광지 정보등을 표시</li>
+          <li className="my-1">[기술 스택] AndroidStudio, Intelij, JAVA, XML, RetrofitAPI, JWTToken</li>
+        </div>
+        <a href="https://github.com/dydgh142/BERMUDA_Android" target="_blank" className="textbutton text-lg">
+          프로젝트 BERMUDA의 GitHub
+        </a>
+      </section>
+      <section className="bg-orange-100">
+        <h2 className="w-[70%] mx-auto text-3xl text-textbase font-semibold py-5">🏃‍♀️ Activities</h2>
+      </section>
+      <section className="w-[70%] mx-auto py-8 pb-14 border-b-2 border-gray-200 ">
+        <h3 className="text-2xl text-textbase font-semibold mb-2">안양대학교 이공대학 학생회</h3>
+        <div className="font-bold text-2xl text-textbase mb-2">Description</div>
+        <div className="border-l-[3px] pl-4 border-textbase mb-5">2017.03. - 2018.02. </div>
+        <div className="font-bold text-xl text-textbase mb-2">기획차장</div>
+        <div className="mb-2 border-l-[3px] border-textbase pl-3">
+          여러 부서중 기획부에 속하여 기획차장의 임무를 수행하였습니다. 매주 인스타그램이나 페이스북 등 SNS에 올라가는 공지사항과 학식 메뉴등을 올리고 학우들의 질문에 답하는 등. 소통을 담당하였습니다. 또한 각종 행사를 진행하게 될때 기획안 작성, 행사
+          진행등을 도맡아 하였습니다.{" "}
+        </div>
+      </section>
+      <section className="w-[70%] mx-auto py-8 pb-14 border-b-2 border-gray-200 ">
+        <h3 className="text-2xl text-textbase font-semibold mb-2">멋쟁이 사자처럼 Front-End School - 4기</h3>
+        <div className="font-bold text-2xl text-textbase mb-2">Description</div>
+        <div className="border-l-[3px] pl-4 border-textbase mb-5">2022.11. - 2023.03. </div>
+        <div className="font-bold text-xl text-textbase mb-2">Summary</div>
+        <div className="mb-4 border-l-[3px] border-textbase pl-3">
+          멋쟁이 사자처럼 스쿨은 주 5일간 ( 8 to 6 ) 총 8시간씩 온라인으로 운영되는 KDT스쿨입니다. 온라인이지만 선생님과 학생들간의 커뮤니케이션이 활발하게 이루어지며 Front-End를 집중적으로 배울 수 있는 환경을 만들어 줍니다.
+        </div>
+        <div className="font-bold text-xl text-textbase mb-2">학습커리큘럼</div>
+        <li className="my-1">HTML/CSS</li>
+        <li className="ml-10 list-[circle]">Git, HTML5, CSS3, RWD, Sass, PostCSS</li>
+        <li className="my-1">JavaScript</li>
+        <li className="ml-10 list-[circle]">Fundamentals, DOM스크립팅</li>
+        <li className="my-1">중간프로젝트 (VanliaJS - 'TAING')</li>
+        <li className="my-1">React</li>
+        <li className="ml-10 list-[circle]">Building User Interface, Redux, Recoil, CSR,SSR, 라우팅</li>
+        <li className="my-1">최종프로젝트 (React - '우동집')</li>
+      </section>
     </div>
   );
 };
